aad41c610b4e0c9f439140bf68c344412f720aac
lelik26 <97464582+lelik26@users.noreply.github.com>
2025-03-10 07:51:20 +0800

    update base py

--- database/base.py
+++ database/base.py
@@ -2,7 +2,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL="postgresql://postgres:mysecretpassword@localhost:5432/mydatabase"
+# DATABASE_URL="postgresql://postgres:mysecretpassword@localhost:5432/mydatabase"
+DATABASE_URL="postgresql://postgres:UptJgqycfITrDcWhBWaoXWQtbnviNoNk@caboose.proxy.rlwy.net:45718/railway"
 
 engine = create_engine(DATABASE_URL, echo=False, future=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
